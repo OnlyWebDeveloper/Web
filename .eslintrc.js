fc04013d5e8850f2db5c00017c801ad6196885f6
OnlyWebDeveloper <56382109+OnlyWebDeveloper@users.noreply.github.com>
2019-12-27 00:04:32 +0200

    Web

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2018,
     },
-    "rules": {
-    }
+    rules: {},
 };
